8a5467e5ce040a6523621793f16041941c66d7bb
Rubens Inojosa <rubensinojosa@hotmail.com>
2024-01-03 12:25:03 -0300

    Chile adjusts

---
+++ app/(countries)/cl/(mobility)/conductor/ciudades/layout.tsx
@@ -0,0 +1,24 @@
+import SmartBanner from "@/components/SmartBanner";
+
+export default async function MXLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      {children}
+      <SmartBanner
+        countryCode="cl"
+        title="DiDi Conductor"
+        desc="Ganancias Extras"
+        btnType="drv"
+        btnMode="primary"
+        type="drv"
+        btnText="Regístrate"
+        textColor="gray-primary"
+        bgColor="bg-white"
+      ></SmartBanner>
+    </>
+  );
+}
