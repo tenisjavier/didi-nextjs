dff782d78ffe4c620ee51e9073e7ee75082630e5
tenisjavier <tenis.javier@gmail.com>
2024-01-08 12:18:19 -0300

    cl - hreflang and canonical init

--- app/(countries)/cl/(mobility)/conductor/page.tsx
+++ app/(countries)/cl/(mobility)/conductor/page.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import BuilderComponent from "@/components/BuilderComponent";
 import { Metadata } from "next";
 import { fetchPageComponents } from "@/utils/db";
-
+import { hreflangs } from "@/config/seo/hreflang";
 //? builder will return the array of components fetch by db by pathname
 
 export const metadata: Metadata = {
   title: "DiDi Conductor - Maneja y Genera Dinero | DiDi Chile",
   description:
     "DiDi Conductor. Revisa los requisitos. Regístrate como socio conductor en las categorías express, taxi y delivery, ganando más y manejando menos.",
+  alternates: {
+    canonical: `https://web.didiglobal.com/cl/conductor/`,
+    languages: hreflangs.drv,
+  },
 };
 
 const Conductor = async () => {
