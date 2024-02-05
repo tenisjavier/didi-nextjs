1f5a61908f050174442289eda7010df96444e6d9
unknown <sluchsinger@uc.cl>
2024-02-05 11:13:34 -0300

    do agregar hrefs

--- app/(countries)/do/(mobility)/sobre-didi/page.tsx
+++ app/(countries)/do/(mobility)/sobre-didi/page.tsx
@@ -8,8 +8,10 @@ import { fetchPageComponents } from "@/utils/db";
 export const metadata: Metadata = {
   title: "Regístrate como Socio Conductor",
   description: "Elmejor",
+  alternates: {
+    canonical: `https://web.didiglobal.com/do/sobre-didi/`,
+  },
 };
-
 
 const SobreDiDi = async () => {
   const components = await fetchPageComponents("/do/sobre-didi/");
@@ -17,4 +19,3 @@ const SobreDiDi = async () => {
 };
 
 export default SobreDiDi;
-
