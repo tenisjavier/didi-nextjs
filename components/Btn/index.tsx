2ef6712cd676d5598856517839aa5780175246fa
tenisjavier <tenis.javier@gmail.com>
2024-02-05 11:52:57 -0300

    fix btn

--- components/Btn/index.tsx
+++ components/Btn/index.tsx
@@ -159,10 +159,3 @@ const Btn = ({
 };
 
 export default Btn;
-
-//? get cookie function
-function getCookie(name: string) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts?.pop()?.split(";").shift();
-}
