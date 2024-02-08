2e6b394dc63b9293a4ca70fa4b16edc195e772e8
unknown <sluchsinger@uc.cl>
2024-02-08 11:21:21 -0300

    minor fixes

--- app/(countries)/cr/(food)/food/restaurantes/preguntas-frecuentes/page.tsx
+++ app/(countries)/cr/(food)/food/restaurantes/preguntas-frecuentes/page.tsx
@@ -6,13 +6,16 @@ import { fetchPageComponents } from "@/utils/db";
 //? builder will return the array of components fetch by db by pathname
 
 export const metadata: Metadata = {
-  title: "Regístrate como Socio Conductor",
-  description: "Elmejor",
+  title: "Términos y Condiciones y Políticas de Privacidad. | DiDi Costa Rica",
+  description:
+    "Conocé nuestros términos y condiciones para el uso del sitio web además de nuestras políticas de privacidad.",
 };
 
 const page = async () => {
-  const components = await fetchPageComponents("/cr/food/restaurantes/preguntas-frecuentes/");
-  console.log('components', components)
+  const components = await fetchPageComponents(
+    "/cr/food/restaurantes/preguntas-frecuentes/"
+  );
+  console.log("components", components);
   return <BuilderComponent components={components}></BuilderComponent>;
 };
 
