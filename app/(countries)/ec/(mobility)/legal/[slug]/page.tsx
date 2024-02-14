f499c205cf12f150950e06a4029bfa66e8c81bd9
unknown <sluchsinger@uc.cl>
2024-02-14 12:41:03 -0300

    build working

---
+++ app/(countries)/ec/(mobility)/legal/[slug]/page.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { fetchLegalBySlug } from "@/utils/db";
+import { notFound } from "next/navigation";
+import RichContent from "@/components/RichContent";
+
+interface FAQProps {
+  params: {
+    slug: string;
+  };
+}
+
+// or Dynamic metadata
+export async function generateMetadata({ params: { slug } }: FAQProps) {
+  const legal = await fetchLegalBySlug("ec", slug);
+  const content = legal.content.json.content[0].content[0].value;
+  return {
+    title: legal.name + " | DiDi Ecuador",
+    description: content.slice(0, 150),
+  };
+}
+
+const Legal = async ({ params: { slug } }: FAQProps) => {
+  const legal = await fetchLegalBySlug("ec", slug);
+  if (!legal) return notFound();
+  return (
+    <section className="container mx-auto mb-32 text-gray-primary md:px-28 pt-16">
+      <RichContent richContent={legal.content}></RichContent>
+    </section>
+  );
+};
+
+export default Legal;
