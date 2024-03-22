b5183de85804350fe3dc23cdddcf6d8eee4a2f6e
Arthury10 <arthurropke@hotmail.com>
2024-03-22 15:44:17 -0300

    fix redirect with params to other site

--- components/Menu/DropdownItem.tsx
+++ components/Menu/DropdownItem.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Link from "next/link";
 import { CountryCode } from "@/typings";
-//import gtmEvent from "@/config/tracking/gtm";
 
 interface DropdownItemProps {
   url: string;
@@ -16,33 +15,30 @@ const DropdownItem = ({
   countryCode,
   handleMenuClick,
 }: DropdownItemProps) => {
-  // const isCTA = url?.includes("onelink");
 
-  // const handleItemClick = (
-  //   e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
-  //   countryCode: CountryCode
-  // ) => {
-  // e.preventDefault();
-  // const link = e.target.href;
-  // const form = link.includes("quickbolt") ? "quickbolt" : "h5";
-  // gtmEvent(`click-btn`, {
-  //   btnType: "menu-link",
-  //   btnLink: link,
-  //   form: form,
-  //   btnText: e.target.innerText,
-  //   countryCode: countryCode,
-  // });
-  // navigate(link);
-  // };
+  const isInternalPages = url.startsWith('/')
 
   return (
-    <Link
-      className="flex h-12 w-full pl-[35%] lg:pl-6 items-center hover:bg-[#d9d9d9] transition-all"
-      href={url}
-      onClick={() => handleMenuClick()} //{isCTA ? (e): void => handleItemClick(e, countryCode) : undefined}
-    >
-      {text}
-    </Link>
+    <>
+      {isInternalPages ? (
+        <Link
+          className="flex h-12 w-full pl-[35%] lg:pl-6 items-center hover:bg-[#d9d9d9] transition-all"
+          href={url}
+          onClick={() => handleMenuClick()} //{isCTA ? (e): void => handleItemClick(e, countryCode) : undefined}
+        >
+          {text}
+        </Link>
+      ) : (
+        <a
+          className="flex h-12 w-full pl-[35%] lg:pl-6 items-center hover:bg-[#d9d9d9] transition-all"
+          href={url}
+          onClick={() => handleMenuClick()} //{isCTA ? (e): void => handleItemClick(e, countryCode) : undefined}
+        >
+          {text}
+        </a>
+      )}
+    </>
+
   );
 };
 
