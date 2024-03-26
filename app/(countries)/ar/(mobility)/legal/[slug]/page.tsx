aea4c54bb40143e3d08d6b3682ef2bf9c445f21a
unknown <sluchsinger@uc.cl>
2024-03-26 13:35:44 -0300

    ar legal slug

---
+++ app/(countries)/ar/(mobility)/legal/[slug]/page.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { fetchLegalBySlug, fetchLegals } from "@/utils/db";
+import { notFound } from "next/navigation";
+import RichContent from "@/components/RichContent";
+import { CountryCode } from "@/typings";
+
+interface LegalProps {
+  params: {
+    slug: string;
+  };
+}
+
+// or Dynamic metadata
+export async function generateMetadata({ params: { slug } }: LegalProps) {
+  const legal = await fetchLegalBySlug("ar", slug);
+  const content = legal.content.json.content[0].content[0].value;
+  return {
+    title: legal.name + " | DiDi Argentina",
+    description: content.slice(0, 150),
+  };
+}
+
+async function generateLegalStaticParams(countryCode: CountryCode) {
+  const legals = (await fetchLegals(countryCode)).items;
+  const legalSlugs = legals.map((legal: any) => {
+    slug: legal.slug;
+  });
+  return legalSlugs;
+}
+
+export async function generateStaticParams() {
+  const legalSlugs = await generateLegalStaticParams("ar");
+  return legalSlugs;
+}
+
+const Legal = async ({ params: { slug } }: LegalProps) => {
+  const legal = await fetchLegalBySlug("ar", slug);
+  if (!legal) return notFound();
+  return (
+    <>
+      <section className="container mx-auto mb-32 text-gray-primary md:px-28 pt-16">
+        <RichContent richContent={legal.content}></RichContent>
+      </section>
+    </>
+  );
+};
+
+export default Legal;
