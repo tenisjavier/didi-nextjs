82258757c446faef3f67a77928b680c648385b49
Rubens Inojosa <rubensinojosa@hotmail.com>
2024-03-26 18:51:56 +0000

    Privacy Policy Page

---
+++ app/(countries)/au/(mobility)/legal/privacy-policy/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import BuilderComponent from "@/components/BuilderComponent";
+import { Metadata } from "next";
+import { fetchPageComponents } from "@/utils/db";
+
+//? builder will return the array of components fetch by db by pathname
+
+export const metadata: Metadata = {
+  title: "Legal | DiDi Australia",
+  description: "Review our privacy policy and other terms and conditions.",
+  alternates: {
+    canonical: `https://web.didiglobal.com/au/legal/privacy-policy`,
+  },
+};
+
+const Legal = async () => {
+  const components = await fetchPageComponents("/au/legal/privacy-policy");
+  return <BuilderComponent components={components}></BuilderComponent>;
+};
+
+export default Legal;
